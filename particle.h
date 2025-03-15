57167cbfed304d8209d5d7ec492fada4513906f9
RajBrarbit <rajnoorbrar0018@gmail.com>
2025-03-14 21:44:34 -0700

    Update particle.h

--- particle.h
+++ particle.h
@@ -1,1 +1,11 @@
 //this is Raj's section (Part B on Milestone 1)
+#include <iostream>
+
+class MovementType { STREAMER, BALLISTIC, FIREWORK};
+
+class Particle {
+public:
+int x,y ;
+int VelocityX, velocityY;
+int lifetime;
+MovementType type;
