68ba345e27c9b9485866848c65174cbe29847196
Aediell Duran <acydurannillo@gmail.com>
2025-03-17 13:32:08 -0700

    Added getters, setters, and blank constructor to particleGraphics

--- particleGraphics.h
+++ particleGraphics.h
@@ -1,2 +1,17 @@
 //This is Duran's section (Part D on Milestone 1)
 #pragma once
+
+class particleGraphics {
+	double particle;
+
+	public:
+		particleGraphics(){
+			//Write smth here later
+		}
+		void setParticle(int p){
+			particle = p;
+		}
+		int getParticle() {
+			return particle;
+		}
+};
