0238415eeaf0535771cf27aefdb1ede03ffe2586
abumaali_1002478 <1002478@my.scccd.edu>
2025-03-17 00:07:48 -0700

    prepped to start test cases

--- test.cc
+++ test.cc
@@ -7,7 +7,15 @@ using namespace std;
 
 void test() {
 	ParticleSystem mySystem;
-	Particle p(1, 1, 1, 1, 1, "test");
-	mySystem.addParticle(p);
-	cout << mySystem.numParticles() << endl;
+	//Particle p(1, 1, 1, 1, 1, "test");
+	//mySystem.addParticle(p);
+	//cout << mySystem.numParticles() << endl;
+	cout << "This is where the different functions will be tested\nPick a number to try things out\n";
+	int choice = 0;
+	while (cin) {
+		cout << "1. Add a particle to the Particle System(will be added to the head of the linked list)\n2. Print particle at head\n3. Print particle at tail\n4. Print the names of all the particles in the Particle System (linked list print)\n";
+		cin >> choice;
+		if (choice == 1) cout << "testing..\n";
+	}
+	
 }
