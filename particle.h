47117f380c767ad1617a94a3a742e668dfc26860
brar_0973673 <Rbrar15@my.scccd.edu>
2025-03-21 12:10:39 -0700

    test

--- particle.h
+++ particle.h
@@ -5,7 +5,8 @@
 #include <cstring>
 #include <cmath>
 using namespace std;
-
+//class ParticleSystem;
+//extern ParticleSystem p;  // Global instance
 
 class Particle {
 	public:
@@ -13,50 +14,62 @@ class Particle {
 		double velocityX, velocityY;
 		double lifetime;
 		string MovementType; // STREAMER , BALLISTIC, FIREWORK
-		// Constructor
+							 // Constructor
 		Particle(double x = 0, double y = 0,double vx = 0,double vy = 0,double life = 0, string type  = "empty")
 			: positionX(x),positionY(y), velocityX(vx), velocityY(vy), lifetime(life), MovementType(type){
 			}
 		// To set movement type
 
 		/*void setMovementType(const char* type){
-			strncpy(MovementType, type, sizeof(MovementType) -1);
-			MovementType[sizeof(MovementType) -1] ='\0';
-		}*/
+		  strncpy(MovementType, type, sizeof(MovementType) -1);
+		  MovementType[sizeof(MovementType) -1] ='\0';
+		  }*/
 
 		void physics() {
-    // Basic physics logic
-    positionX += velocityX;
-    positionY += velocityY;
+			// Basic physics logic
+			positionX += velocityX;
+			positionY += velocityY;
 
-    if (MovementType == "BALLISTIC") {
-        velocityY += 1.0;
-    } else if (MovementType == "FIREWORK") {
-        if (lifetime <= 0) {
-			explode();
-		}
-	}
-	else if (MovementType == "STREAMER"){
-	}
-	lifetime -=1;
+			if (MovementType == "BALLISTIC") {
+				velocityY += 1.0;
+			} else if (MovementType == "FIREWORK") {
+				if (lifetime <= 0) {
+					explode();
+				}
+			}
+			if (MovementType == "STREAMER") {
+				if (std::abs(velocityX) > std::abs(velocityY)) {
+					velocityY = 0;  // Move only horizontally
+				} else {
+					velocityX = 0;  // Move only vertically
+				}
+			}
+			lifetime -=1;
 		}
 
 		void explode(){
-			cout << "Firework exploded into 8 streamers!\n";
-			const int numParticles = 8;
-			for (int i = 0; i< numParticles; ++i){
-				double angle = i * 45.0 * (3.14159265 / 180.0);
-				double speed = 5.0;
-				double vx = cos(angle) * speed;
-				double vy = sin(angle) * speed;
-				Particle streamer(positionX, positionY,vx, vy, 30, "STREAMER");
-				streamer.draw();
-			}
-		}
 
-		void draw() const {
-			std::cout << "Drawing particle at ( " << positionX << "," << positionY << ") of type " << MovementType << "\n";
+			// New particles have half the lifetime of the original
+			int newLifetime = lifetime / 2;
+			if (newLifetime <= 0) newLifetime = 1;  // Ensure particles last at least 1 frame
+
+			// Define 4 break-off directions (right, left, up, down)
+			double speed = 3.0; // Moderate speed
+			double directions[4][2] = {
+				{speed, 0},  // Right
+				{-speed, 0}, // Left
+				{0, -speed}, // Up
+				{0, speed}   // Down
+			};
+
+			// Create and add 4 new particles to the linked list
+			/*for (int i = 0; i < 4; ++i) {
+				p.addParticle(Particle(positionX, positionY, directions[i][0], directions[i][1], newLifetime, "FRAGMENT"));
+			}*/
 		}
+		/*	void draw() const {
+			std::cout << "Drawing particle at ( " << positionX << "," << positionY << ") of type " << MovementType << "\n";
+			}*/
 
 		void printDetails() const{
 			std::cout << "Particle Details:\n";
