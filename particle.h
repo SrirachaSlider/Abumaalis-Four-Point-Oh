5650493bc4039756262872119d18e9e6f103ee53
brar_0973673 <Rbrar15@my.scccd.edu>
2025-03-24 12:03:05 -0700

    milestone3

--- particle.h
+++ particle.h
@@ -1,22 +1,31 @@
 //this is Raj's section (Part B on Milestone 1)
 #pragma once
+#include <vector>
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <cmath>
+#include "/public/colors.h"
 using namespace std;
 //class ParticleSystem;
 //extern ParticleSystem p;  // Global instance
 
 class Particle {
 	public:
-		double positionX, positionY;
-		double velocityX, velocityY;
-		double lifetime;
-		string MovementType; // STREAMER , BALLISTIC, FIREWORK
+		int x, y; // for positions
+		double dx, dy; // for velocities
+		int lifetime;
+		bool apply_gravity;
+		bool make_firework = false;
+		static int screen_width, screen_height; // Store screen size
+
+		//string MovementType; // STREAMER , BALLISTIC, FIREWORK
 							 // Constructor
-		Particle(double x = 0, double y = 0,double vx = 0,double vy = 0,double life = 0, string type  = "empty")
-			: positionX(x),positionY(y), velocityX(vx), velocityY(vy), lifetime(life), MovementType(type){
+		Particle(int start_x, int start_y, double vx, double vy, int life, bool gravity = true)
+			:x(start_x), y(start_y), dx(vx), dy(vy), lifetime(life), apply_gravity(gravity) {
+				auto [rows, cols] = get_terminal_size();
+				rows--;
+				cols--;
 			}
 		// To set movement type
 
@@ -24,59 +33,52 @@ class Particle {
 		  strncpy(MovementType, type, sizeof(MovementType) -1);
 		  MovementType[sizeof(MovementType) -1] ='\0';
 		  }*/
+		/*static void setScreenSize(int width, int height){
+			screen_width = width;
+			screen_height = height;
+		}*/
 
-		void physics() {
+		void physics(){
+
 			// Basic physics logic
-			positionX += velocityX;
-			positionY += velocityY;
+			x += dx;
+			y += dy;
 
-			if (MovementType == "BALLISTIC") {
-				velocityY += 1.0;
-			} else if (MovementType == "FIREWORK") {
-				if (lifetime <= 0) {
-					explode();
-				}
+			//Bounce off edges of screen ballistic
+			if (x < 0){
+				x = 0;
+				dx = -dx;
 			}
-			if (MovementType == "STREAMER") {
-				if (std::abs(velocityX) > std::abs(velocityY)) {
-					velocityY = 0;  // Move only horizontally
-				} else {
-					velocityX = 0;  // Move only vertically
-				}
+			if (x >= screen_width){
+				x = screen_width -1;
+				dx = -dx;
 			}
-			lifetime -=1;
+			if (y < 0){
+				y = 0;
+				dy = -dy;
+			}
+			if (y >= screen_height){
+				y = screen_height -1;
+				dy = -dy;
+			}
+			// firework gravity
+			if (apply_gravity) dy += 1.0;
+
+			//decrement lifetime
+			lifetime--;
+		}
+	
+		bool is_alive() const {
+			return lifetime > 0;
 		}
 
-		void explode(){
+		void explode() const{
+			for (int i = 0; i < 360; i+= 90){ //creating 4 particles
+			double angle = i * M_PI/ 180.0; // convert degree to radian
+			double vx = cos(angle) * 2.0; //explode speed
+			double vy = sin(angle) * 2.0; 
+			//new_particles.push_back(Particle(x,y,vx,vy,30,false)); // streamer particle
 
-			// New particles have half the lifetime of the original
-			int newLifetime = lifetime / 2;
-			if (newLifetime <= 0) newLifetime = 1;  // Ensure particles last at least 1 frame
-
-			// Define 4 break-off directions (right, left, up, down)
-			double speed = 3.0; // Moderate speed
-			double directions[4][2] = {
-				{speed, 0},  // Right
-				{-speed, 0}, // Left
-				{0, -speed}, // Up
-				{0, speed}   // Down
-			};
-
-			// Create and add 4 new particles to the linked list
-			/*for (int i = 0; i < 4; ++i) {
-				p.addParticle(Particle(positionX, positionY, directions[i][0], directions[i][1], newLifetime, "FRAGMENT"));
-			}*/
 		}
-		/*	void draw() const {
-			std::cout << "Drawing particle at ( " << positionX << "," << positionY << ") of type " << MovementType << "\n";
-			}*/
-
-		void printDetails() const{
-			std::cout << "Particle Details:\n";
-			std::cout << "Position: (" << positionX << ", " << positionY << ")\n";
-			std::cout << "velocity: (" << velocityX << ", " << velocityY << ")\n";
-			std::cout << "LifetieL " << lifetime << "\n";
-			std::cout << "Movement Type:" << MovementType << "\n";
-		}
+}
 };
-
