9fbd14caaa50d89bc5f3bec7e906ad1237061614
abumaali_1002478 <1002478@my.scccd.edu>
2025-03-25 13:07:23 -0700

    prepped most of mohammad.cc

--- mohammad.cc
+++ mohammad.cc
@@ -8,22 +8,37 @@
 using namespace std;
 
 void mohammad() {
-	/*cout << "This is Mohammad's portion\n";
-	int x = 0;
-	cout << "Enter a number to square:\n";
-	cin >> x;
-	cout << x << " squared is: " << x * x << endl;*/
 	ParticleSystem mySystem;
-	Particle p(5, 5, 1, 1, 50);
-	Particle c(30, 30, 2, 2, 50);
-	Particle d(10, 1, 3, 3, 50);
-	mySystem.addParticle(p);
-	mySystem.addParticle(c);
-	mySystem.addParticle(d);
-	mySystem.drawParticles();
-	//mySystem.moveParticles();
-	//usleep(1000000);
-	//resetcolor();
-	//clearscreen();
-	movecursor(55,1);
+	int choice = -1;
+	while (cin) {
+		cout << "Enter a number that corresponds with one of the following options below:\n";
+		cout << "0. Quit\n1. Add a particle\n2. Draw particles\n3. Move particles\n";
+		cin >> choice;
+		if (choice == 0) break;
+		if (choice == 1) {
+			int x = -1, y = -1, xv = -1, yv = -1, lifetime = -1;
+			string type;
+			cout << "Enter values for the X and Y position (separately), the X and Y velocity (separately), the lifetime, and the type of the particle\n(If you do not want the particle to have a type just enter anything other than Streamer, Ballistic, or Firework)\n*Note: If you make your particle a type Streamer, but your inputs say otherwise, it will be made a Streamer\n";
+			cout << "X and Y position: ";
+			cin >> x >> y;
+			cout << "X and Y velocity: ";
+			cin >> xv >> yv;
+			cout << "Lifetime: ";
+			cin >> lifetime;
+			cout << "Type: ";
+			cin >> type;
+			Particle p(x, y, xv, yv, lifetime, type);
+			mySystem.addParticle(p);
+			cout << "Particle added to the Particle System.\n";
+		}
+		if (choice == 2) {
+			mySystem.drawParticles();
+			movecursor(mySystem.rows, 1);
+		}
+		if (choice == 3) {
+			mySystem.moveParticles();
+			movecursor(mySystem.rows, 1);
+		}
+	}
+
 }
