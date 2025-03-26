bf27ad8d2be87626ada8d3dd9f495cf775b8274b
f <f>
2025-03-25 23:05:41 -0700

    particles added/move

--- michael.cc
+++ michael.cc
@@ -7,5 +7,17 @@
 using namespace std;
 
 void michael() {
-	cout << "This is Michael's portion\n";
+	//cout << "This is Michael's portion\n";
+	ParticleSystem swag;
+
+	for (int i = 0; i < 5; i++) {
+		int x = rand() % swag.get_cols();
+		int y = rand() % swag.get_rows();
+		swag.addParticle(Particle(x, y, 0, -3, 50, "firework"));
+	}
+
+	for (int i = 0; i < 100; i++) {
+		swag.moveParticles();
+		usleep(100'000);
+	}
 }
